202579ec54ab434095fb6f16c1eb70efa97cc1ac
natalieoshea <ms.natalieoshea@gmail.com>
2021-04-17 19:30:16 -0400

    response rate data documentation

---
+++ R/data.R
@@ -0,0 +1,207 @@
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census for NYC assembly districts.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 11,115 rows and 6 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{GEO_ID}{assembly district}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"assembly"
+
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census for NYC boroughs.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 855 rows and 6 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{GEO_ID}{borough}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"borough"
+
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census for NYC community board districts.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 10,089 rows and 6 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{GEO_ID}{community board district}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"commBoard"
+
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census for NYC congressional districts.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 2,223 rows and 6 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{GEO_ID}{congressional district}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"congressional"
+
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census for NYC council districts.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 8,721 rows and 6 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{GEO_ID}{council district}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"council"
+
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census for NYC zip codes (modified zip code.
+#'  tabulation areas). The variables are as follows:
+#'
+#' @format A data frame with 30,096 rows and 6 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{GEO_ID}{modzcta district}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"modzcta"
+
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census for NYC neighborhoods.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 42,750 rows and 8 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{GEO_ID}{neighborhood code (nCode)}
+#'   \item{BORO}{borough}
+#'   \item{NEIGHBORHOOD}{neighborhood}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"nCode"
+
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census for NYC.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 171 rows and 5 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"nyc"
+
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census in NYC school districts.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 5,472 rows and 6 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{GEO_ID}{school district}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"school"
+
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census in NYC state senate districts.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 4,446 rows and 6 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{GEO_ID}{state senate district}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"stateSenate"
+
+
+#' Daily Response Rate Data for the 2020 Census in New York City
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census in NYC census tracts (2020 geography).
+#'  The variables are as follows:
+#'
+#' @format A data frame with 383,541 rows and 9 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{GEO_ID}{census tract (2020 geography)}
+#'   \item{BORO}{borough}
+#'   \item{NEIGHBORHOOD}{neighborhood}
+#'   \item{TRACT}{tract}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"tract_2020"
+
+#' Daily Response Rate Data for the 2020 Census in the United States
+#'
+#' A dataset containing daily response rate data (March 20 to October 17) reported
+#'  by the US Census Bureau for the 2020 Census for the US.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 11,115 rows and 6 variables:
+#' \describe{
+#'   \item{RESP_DATE}{response date (format: YYYY-MM-DD)}
+#'   \item{CRRALL}{cumulative response rate}
+#'   \item{CRRINT}{cumulative response rate (internet-only)}
+#'   \item{DRRALL}{daily response rate}
+#'   \item{DRRINT}{daily response rate (internet-only)}
+#' }
+"us"
