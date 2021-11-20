1c5a3bf94cdd88d18333a3070cb5cd7fef098ce3
natalieoshea <ms.natalieoshea@gmail.com>
2021-11-20 17:45:27 -0500

    format data for datatable

--- inst/shiny-examples/nyccensus2020/server.R
+++ inst/shiny-examples/nyccensus2020/server.R
@@ -1,10 +1,27 @@
 function(input, output, session) {
 
+  # App settings ----
+
+  # share input across tabs
+  observe({
+    geo_map <- input$geo
+    updateSelectInput(session, "geo2", selected = geo_map)
+  })
+
+  observe({
+    geo_table <- input$geo2
+    updateSelectInput(session, "geo", selected = geo_table)
+  })
+
+  census_data <- reactive({
+    get(paste0("rr_", input$geo))
+  })
+
   # Map ----
 
   map_df <- reactive({
     get(paste0("geo_", input$geo)) %>%
-      left_join(get(paste0("rr_", input$geo))) %>%
+      left_join(census_data()) %>%
       filter(RESP_DATE == input$date)
   })
 
@@ -44,12 +61,71 @@ function(input, output, session) {
   # Data table ----
 
   output$data_table <- DT::renderDataTable({
-    map_df() %>%
+
+    # save crosswalk info
+    crosswalk <- crosswalk_tract_2020 %>%
+      select(input$geo2, borough, if(input$geo2 %in% c("nCode", "tract_2020")) "neighborhood") %>%
+      unique()
+
+    # save GEO_ID label info
+    label <- case_when(input$geo2 == "nCode" ~ "Code",
+                       input$geo2 == "tract_2020" ~ "Census Tract",
+                       input$geo2 == "borough" ~ "Borough",
+                       input$geo2 == "congressional" ~ "Congressional District",
+                       input$geo2 == "stateSenate" ~ "State Senate District",
+                       input$geo2 == "council" ~ "City Council District",
+                       input$geo2 == "assembly" ~ "Assembly District",
+                       input$geo2 == "school" ~ "School District",
+                       input$geo2 == "commBoard" ~ "Community Board District",
+                       input$geo2 == "modzcta" ~ "Zip Code")
+
+    # wrangle datatable info
+    filtered_df <- census_data() %>%
+      # filter to selected date range
+      filter(RESP_DATE >= min(input$dateRange) & RESP_DATE <= max(input$dateRange)) %>%
+      # select relevant columns
+      select(RESP_DATE, GEO_ID, CRRALL, CRRINT, DRRALL, DRRINT) %>%
+      # add crosswalk info
+      left_join(crosswalk, by = c("GEO_ID" = input$geo2)) %>%
+      # filter to selected borough(s)
+      filter(is.null(input$boro) | if (input$geo2 == "borough") {GEO_ID %in% input$boro} else {borough %in% input$boro}) %>%
+      # rename columns
+      rename("Date" = "RESP_DATE",
+             "Cumulative Response Rate" = "CRRALL",
+             "Cumulative Response Rate (Internet)" = "CRRINT",
+             "Daily Response Rate" = "DRRALL",
+             "Daily Response Rate (Internet)" = "DRRINT")
+
+    # order and name columns based on user input
+    table_df <- if(input$geo2 == "borough") {
+      filtered_df %>%
+        select(Date, GEO_ID, everything())
+    } else if(input$geo2 == "nCode") {
+      filtered_df %>%
+        select(Date, borough, neighborhood, everything())
+    } else if(input$geo2 == "tract_2020") {
+      filtered_df %>%
+        select(Date, borough, neighborhood, everything()) %>%
+        # remove tract prefix
+        mutate(GEO_ID = str_remove(GEO_ID, "1400000US360"))
+    } else {
+      filtered_df %>%
+        select(Date, borough, everything())
+    }
+
+    # generate datatable
+    table_df %>%
+      # rename GEO_ID column with correct label
+      rename(!!paste0(label) := "GEO_ID") %>%
+      # make sure all column names are in title case
+      rename_all(str_to_title) %>%
+      # create DT object
       DT::datatable(rownames = FALSE, extensions = "Buttons",
                     options = list(dom = "Blrtip",
                                    buttons = c("copy", "csv", "pdf"),
                                    lengthMenu = list(c(10, 25, 50, -1),
                                                      c(10, 25, 50, "All"))))
+
   })
 
 }
