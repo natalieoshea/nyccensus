aec34a92437b5bd6b4fcbcfa86d7b7854edc3ad5
natalieoshea <ms.natalieoshea@gmail.com>
2021-11-22 19:46:02 -0500

    highlight clicked polygon

--- inst/shiny-examples/nyccensus2020/server.R
+++ inst/shiny-examples/nyccensus2020/server.R
@@ -26,17 +26,22 @@ function(input, output, session) {
       filter(RESP_DATE == input$date)
   })
 
-
-  output$map <- renderLeaflet({
-    pal <- colorNumeric("viridis", map_df()[[input$var]])
-    var <- map_df()[[input$var]]
-
+  # set base map
+  output$map <-  renderLeaflet({
     leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
       addProviderTiles(
         provider = "CartoDB.Positron",
         options = providerTileOptions(minZoom = 10, maxZoom = 20)
       ) %>%
-      setView(lng = -74.05, lat = 40.71, zoom = 11) %>%
+      setView(lng = -74.05, lat = 40.71, zoom = 11)
+  })
+
+  # add colors to polygons
+  observe({
+    pal <- colorNumeric("viridis", map_df()[[input$var]])
+    var <- map_df()[[input$var]]
+
+    leafletProxy("map", data = map_df()) %>%
       addPolylines(
         data = map_df(),
         weight = 1,
@@ -72,10 +77,52 @@ function(input, output, session) {
     print(map_click)
   })
 
-  # pair programming steps:
-  # convert map to leafletProxy for better compatibility with Shiny reactivity
   # store map_shape_click ID as a reactive value
-  # add highlighted polylines to leafletProxy object
+  clickedShape <- reactiveVal(NA)
+
+  observeEvent(input$map_click, {
+    if (is.null(input$map_shape_click)) {
+      # if input#map_shape_click hasn't been initiated, set clickedShape() to NA
+      clickedShape(NA)
+    } else if (input$map_click$lat == input$map_shape_click$lat &
+               input$map_click$lng == input$map_shape_click$lng) {
+      # save polygon value
+      val <- map_df() %>% filter(GEO_ID == input$map_shape_click$id) %>% pull("CRRALL")
+      # if value is NA, set clickedShape() to NA
+      ifelse(is.na(val), clickedShape(NA), clickedShape(input$map_shape_click$id))
+    } else {
+      # if user clicks outside a polygon, set clickedShape() to NA
+      clickedShape(NA)
+    }
+  })
+
+  # if user switches geographies, set clickedShape() to NA
+  observeEvent(input$geo, {
+    clickedShape(NA)
+  })
+
+  # highlight clickedShape() polygon
+  observeEvent(clickedShape(), {
+
+    if (!is.na(clickedShape())) {
+      # save clickedShape() geometry
+      clicked_polygon <- map_df()$geometry[map_df()$GEO_ID %in% clickedShape()]
+
+      # add highlight polylines
+      leafletProxy("map") %>%
+        clearGroup("highlights") %>%
+        addPolylines(data = clicked_polygon,
+                     stroke = TRUE,
+                     weight = 5,
+                     color = "darkblue",
+                     group = "highlights")
+    } else {
+      # remove previously highlighted polylines
+      leafletProxy("map") %>%
+        clearGroup("highlights")
+    }
+
+  })
 
   # Data table ----
 
